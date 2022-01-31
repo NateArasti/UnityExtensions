e3a980b9b4c89e696babf20a1e6c97f6b46edfa9
Arasti <arastigames@gmail.com>
2022-01-31 16:06:55 +0500

    New Extension Methods

--- UnityExtensions.cs
+++ UnityExtensions.cs
@@ -1,23 +1,27 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public static class UnityExtensions
 {
-    //Storing Main Camera
     private static Camera _mainCamera;
-    public static Camera MainCamera 
+    ///<summary>
+    /// Stored Main Camera
+    ///</summary>
+    public static Camera MainCamera
     {
-        get 
+        get
         {
-            if(_mainCamera == null) _mainCamera = Camera.main;
+            if (_mainCamera == null) _mainCamera = Camera.main;
             return _mainCamera;
         }
     }
 
-    //Non-allocating WaitForSeconds
-    private static readonly Dictionary<float, WaitForSeconds> WaitDictionary = 
+    private static readonly Dictionary<float, WaitForSeconds> WaitDictionary =
         new Dictionary<float, WaitForSeconds>();
-
+    ///<summary>
+    /// Non-allocating WaitForSeconds
+    ///</summary>
     public static WaitForSeconds Wait(float waitTime)
     {
         if (WaitDictionary.TryGetValue(waitTime, out var waitForSeconds))
@@ -29,23 +33,66 @@ public static class UnityExtensions
         return WaitDictionary[waitTime];
     }
 
-    //World Position of Canvas element
+    ///<summary>
+    ///World Position of Canvas element
+    ///</summary>
     public static Vector2 GetWorldPositionOfCanvasElement(RectTransform rectTransform)
     {
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            rectTransform, 
-            rectTransform.position, 
+            rectTransform,
+            rectTransform.position,
             MainCamera,
             out var result);
         return result;
     }
 
-    //Destroy all kids of object
+    ///<summary>
+    ///Destroy all kids of object
+    ///</summary>
     public static void DestroyChildren(this Transform transform)
     {
         foreach (Transform childTransform in transform)
         {
             Object.Destroy(childTransform.gameObject);
+        }
+    }
+
+    ///<summary>
+    ///Change alpha of Sprites to create Fade effect
+    ///</summary>
+    public static void Fade(this SpriteRenderer spriteRenderer, float alpha)
+    {
+        var color = spriteRenderer.color;
+        color.a = alpha;
+        spriteRenderer.color = color;
+    }
+
+    ///<summary>
+    /// Gets random object from list
+    ///</summary>
+    public static T GetRandomObject<T>(this IList<T> list)
+    {
+        if (list.Count == 0) throw new UnityException("Can't get random object from empty list");
+        return list[Random.Range(0, list.Count)];
+    }
+
+    ///<summary>
+    /// Remove random object from list
+    ///</summary>
+    public static void RemoveRandomObject<T>(this IList<T> list)
+    {
+        if (list.Count == 0) throw new UnityException("Can't remove random object from empty list");
+        list.RemoveAt(Random.Range(0, list.Count));
+    }
+
+    ///<summary>
+    /// Call Action for each object in collection
+    ///</summary>
+    public static void ForEachAction<T>(this ICollection<T> collection, UnityAction<T> action)
+    {
+        foreach (var obj in collection)
+        {
+            action.Invoke(obj);
         }
     }
 }

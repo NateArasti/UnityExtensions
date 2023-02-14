23e0bc4f1e11b1c1891aca5e530c62c9b89938ac
Arasti <arastigames@gmail.com>
2023-02-15 00:20:34 +0500

    Update CollectionsExtensions.cs

--- Runtime/CollectionsExtensions.cs
+++ Runtime/CollectionsExtensions.cs
@@ -8,32 +8,32 @@ namespace UnityExtensions
 {
     public static class CollectionsExtensions
     {
-        ///<summary>
-        /// Gets random object from list
-        ///</summary>
-        public static T GetRandomObject<T>(this IList<T> list)
-        {
-            if (list.Count == 0) throw new UnityException("Can't get random object from empty list");
-            return list[Random.Range(0, list.Count)];
-        }
+        /////<summary>
+        ///// Gets random object from list
+        /////</summary>
+        //public static T GetRandomObject<T>(this IList<T> list)
+        //{
+        //    if (list.Count == 0) throw new UnityException("Can't get random object from empty list");
+        //    return list[Random.Range(0, list.Count)];
+        //}
 
-        ///<summary>
-        /// Gets random object from collection
-        /// O(rand_index)
-        ///</summary>
-        public static T GetRandomObject<T>(this ICollection<T> collection)
-        {
-            if (collection.Count == 0) throw new UnityException("Can't get random object from empty collection");
-            var returnIndex = Random.Range(0, collection.Count);
-            var count = 0;
-            foreach (var obj in collection)
-            {
-                if (count == returnIndex) return obj;
-                count += 1;
-            }
+        /////<summary>
+        ///// Gets random object from collection
+        ///// O(rand_index)
+        /////</summary>
+        //public static T GetRandomObject<T>(this ICollection<T> collection)
+        //{
+        //    if (collection.Count == 0) throw new UnityException("Can't get random object from empty collection");
+        //    var returnIndex = Random.Range(0, collection.Count);
+        //    var count = 0;
+        //    foreach (var obj in collection)
+        //    {
+        //        if (count == returnIndex) return obj;
+        //        count += 1;
+        //    }
 
-            return default;
-        }
+        //    return default;
+        //}
         
         ///<summary>
         /// Gets random object from list

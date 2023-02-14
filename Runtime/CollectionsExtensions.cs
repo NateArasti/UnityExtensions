faa3458cf27b558490e87d2b914a08616791da02
Arasti <arastigames@gmail.com>
2023-02-15 00:15:40 +0500

    Update CollectionsExtensions.cs

--- Runtime/CollectionsExtensions.cs
+++ Runtime/CollectionsExtensions.cs
@@ -34,6 +34,33 @@ namespace UnityExtensions
 
             return default;
         }
+        
+        ///<summary>
+        /// Gets random object from list
+        ///</summary>
+        public static T GetRandomObject<T>(this IReadOnlyList<T> list)
+        {
+            if (list.Count == 0) throw new UnityException("Can't get random object from empty list");
+            return list[Random.Range(0, list.Count)];
+        }
+
+        ///<summary>
+        /// Gets random object from collection
+        /// O(rand_index)
+        ///</summary>
+        public static T GetRandomObject<T>(this IReadOnlyCollection<T> collection)
+        {
+            if (collection.Count == 0) throw new UnityException("Can't get random object from empty collection");
+            var returnIndex = Random.Range(0, collection.Count);
+            var count = 0;
+            foreach (var obj in collection)
+            {
+                if (count == returnIndex) return obj;
+                count += 1;
+            }
+
+            return default;
+        }
 
         ///<summary>
         /// Remove random object from list
